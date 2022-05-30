3325de9bd7012fb65c34e6215496e57b8d05c15d
nus3 <hadayota33@gmail.com>
2022-05-30 15:17:05 +0900

    :shower: Remove default vite page

--- apps/react-swc-jest/src/main.tsx
+++ apps/react-swc-jest/src/main.tsx
@@ -1,8 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 
-import App from './App';
-import './index.css';
+import { App } from './App';
+import 'the-new-css-reset/css/reset.css';
 
 // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
 ReactDOM.createRoot(document.getElementById('root')!).render(
