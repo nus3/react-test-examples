7d5a941a2299995bb19d51c5903f8003aba5c09e
nus3 <hadayota33@gmail.com>
2022-05-30 11:51:15 +0900

    :wrench: add eslint and prettier

---
+++ apps/react-swc-jest/.eslintrc.js
@@ -0,0 +1,52 @@
+/** @type {import('@typescript-eslint/experimental-utils').TSESLint.Linter.Config} */
+
+module.exports = {
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 12,
+    sourceType: 'module',
+  },
+  plugins: ['unused-imports'],
+  extends: [
+    'plugin:@typescript-eslint/recommended',
+    'next/core-web-vitals',
+    'prettier',
+  ],
+  rules: {
+    'import/order': [
+      'error',
+      {
+        groups: [
+          'builtin',
+          'external',
+          'internal',
+          ['parent', 'sibling'],
+          'object',
+          'type',
+          'index',
+        ],
+        'newlines-between': 'always',
+        pathGroupsExcludedImportTypes: ['builtin'],
+        alphabetize: { order: 'asc', caseInsensitive: true },
+        pathGroups: [
+          { pattern: 'components/**', group: 'internal', position: 'before' },
+          // styles
+          { pattern: './**.module.css', group: 'index', position: 'before' },
+        ],
+      },
+    ],
+    'unused-imports/no-unused-imports-ts': 'warn',
+    'react/self-closing-comp': [
+      'error',
+      {
+        component: true,
+        html: true,
+      },
+    ],
+    '@next/next/no-img-element': 'off',
+    '@next/next/no-html-link-for-pages': 'off',
+  },
+}
--- apps/react-swc-jest/src/App.tsx
+++ apps/react-swc-jest/src/App.tsx
@@ -1,4 +1,5 @@
 import { useState } from 'react'
+
 import logo from './logo.svg'
 import './App.css'
 
