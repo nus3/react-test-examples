d3b41ea18cab2f06de7d112f51affa4586a1e595
nus3 <hadayota33@gmail.com>
2022-06-27 15:56:54 +0900

    :+1: add keyboard test

---
+++ apps/react-swc-jest/src/components/SelectBox/SelectBox.stories.tsx
@@ -0,0 +1,16 @@
+import { action } from '@storybook/addon-actions';
+
+import { SelectBox } from './SelectBox';
+
+import type { ComponentStoryObj, Meta } from '@storybook/react';
+
+export default {
+  title: 'SelectBox',
+  component: SelectBox
+} as Meta;
+
+export const Default: ComponentStoryObj<typeof SelectBox> = {
+  args: {
+    onClick: action('onClick')
+  }
+};
