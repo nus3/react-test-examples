754c949137f1cf52e283d2b7a60192c58e229899
nus3 <hadayota33@gmail.com>
2022-08-14 14:29:58 +0900

    :+1: add get api test

---
+++ apps/react-swc-jest/src/api/example.ts
@@ -0,0 +1,16 @@
+export type GetExamplesRequest = {
+  name: string;
+};
+
+export type GetExamplesResponse = {
+  examples: Array<{ id: string; name: string }>;
+};
+
+export const getExamples = async (params?: GetExamplesRequest) => {
+  const response = await fetch('/examples', {
+    method: 'GET',
+    body: params && JSON.stringify({ name: params.name })
+  });
+  const formattedResponse = await response.json();
+  return formattedResponse as GetExamplesResponse;
+};
