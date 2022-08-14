a10f59ab53285e6345c0df31446649e04005ef27
nus3 <hadayota33@gmail.com>
2022-08-14 15:16:03 +0900

    :+1: add useFocus test

---
+++ apps/react-swc-jest/src/components/SelectBox/useFocus.test.ts
@@ -0,0 +1,54 @@
+import { act, renderHook } from '@testing-library/react';
+
+import { SELECT_BOX_VALUES } from './SelectBox';
+import { useFocus } from './useFocus';
+
+describe('useFocus', () => {
+  test('should be default focused value is 0', () => {
+    const { result } = renderHook(() => useFocus(SELECT_BOX_VALUES));
+    expect(result.current.focusedValue).toBe(0);
+  });
+
+  test('should update focusedValue when use moveFocus', () => {
+    const { result } = renderHook(() => useFocus(SELECT_BOX_VALUES));
+    expect(result.current.focusedValue).toBe(0);
+
+    act(() => {
+      result.current.moveFocus('positive');
+    });
+    expect(result.current.focusedValue).toBe(1);
+    act(() => {
+      result.current.moveFocus('positive');
+    });
+    expect(result.current.focusedValue).toBe(2);
+    act(() => {
+      result.current.moveFocus('positive');
+    });
+    expect(result.current.focusedValue).toBe(3);
+    act(() => {
+      result.current.moveFocus('positive');
+    });
+    expect(result.current.focusedValue).toBe(0);
+
+    act(() => {
+      result.current.moveFocus('negative');
+    });
+    expect(result.current.focusedValue).toBe(3);
+    act(() => {
+      result.current.moveFocus('negative');
+    });
+    expect(result.current.focusedValue).toBe(2);
+    act(() => {
+      result.current.moveFocus('negative');
+    });
+    expect(result.current.focusedValue).toBe(1);
+    act(() => {
+      result.current.moveFocus('negative');
+    });
+    expect(result.current.focusedValue).toBe(0);
+    act(() => {
+      result.current.moveFocus('negative');
+    });
+    expect(result.current.focusedValue).toBe(3);
+  });
+});
