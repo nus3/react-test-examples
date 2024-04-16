0c41a971b47d25d35745ef85b08943a163c7b379
nus3 <hadayota33@gmail.com>
2024-04-16 13:28:21 +0900

    :pencil2: fix type check

--- apps/react-vitest/src/hooks/useHash.test.ts
+++ apps/react-vitest/src/hooks/useHash.test.ts
@@ -1,5 +1,5 @@
 import { renderHook, waitFor } from "@testing-library/react";
-import { describe, expect, test, vi } from "vitest";
+import { describe, expect, test, vi, beforeEach } from "vitest";
 
 import { useHash } from "./useHash";
 
