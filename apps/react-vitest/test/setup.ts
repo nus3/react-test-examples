6843251cd6714ee46c326cc88dba0ef2ebe5629f
nus3 <hadayota33@gmail.com>
2024-04-17 10:02:42 +0900

    :pencil2: remove whatwg-fetch and use @testing-library/jest-dom/vitest

--- apps/react-vitest/test/setup.ts
+++ apps/react-vitest/test/setup.ts
@@ -1,5 +1,4 @@
-import "@testing-library/jest-dom";
+import "@testing-library/jest-dom/vitest";
 import { vi } from "vitest";
-import "whatwg-fetch"; // To run in jest environment
 
 vi.mock("../src/api/example");
