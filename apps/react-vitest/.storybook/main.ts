741f47064c613fcccef577e18515d27f408df2c6
nus3 <hadayota33@gmail.com>
2024-06-10 17:46:40 +0900

    update storybook

--- apps/react-vitest/.storybook/main.ts
+++ apps/react-vitest/.storybook/main.ts
@@ -28,8 +28,10 @@ const config: StorybookConfig = {
     return config;
   },
 
-  docs: {
-    autodocs: true,
+  docs: {},
+
+  typescript: {
+    reactDocgen: "react-docgen-typescript",
   },
 };
 
